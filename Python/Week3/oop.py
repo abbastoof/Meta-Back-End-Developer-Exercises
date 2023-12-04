eb47404091886d1e1ffcccc3dbc579b0c5fa2c4f
abbastoof <abbas.toof@gmail.com>
2023-12-04 21:26:30 +0200

    oop

--- Python/Week3/oop.py
+++ Python/Week3/oop.py
@@ -47,3 +47,26 @@ class Beta(Alpha): # Inherit from Alpha
 # _class__identifier
 
 # Class is the name of the class and identifier is the data member that I want to access.
+
+
+class House:
+    '''
+    This is a stub for a class representing a house that can be used to create objects and evaluate different metrics that we may require in constructing it.
+    '''
+    num_rooms = 5
+    bathrooms = 2
+
+    def cost_evaluation(self, rate):
+        # Functionality to calculate the costs from the area of the house
+        cost = rate * self.num_rooms
+        return cost
+
+
+
+house = House()
+print(house.num_rooms)
+print(House.num_rooms)
+house.num_rooms = 7
+# House.num_rooms = 7
+print(house.num_rooms)
+print(House.num_rooms)
